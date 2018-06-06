7b1f5f5a16c56d91afb1d4f251eadede01b61bd1
best08618 <best08618@naver.com>
2018-06-06 16:10:31 +0900

    main xml

--- app/src/main/java/com/example/aa/jinjikk_proj/MainActivity.java
+++ app/src/main/java/com/example/aa/jinjikk_proj/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.aa.jinjikk_proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button test = (Button) findViewById(R.id.testButton);
+        Button memory = (Button) findViewById(R.id.memoryButton);
+        Button pitch = (Button) findViewById(R.id.pithButton);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),testcode.class);
+                startActivity(intent);
+            }
+        });
+     }
+
 }
